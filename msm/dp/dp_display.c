270b1bc33a1d74b91b089272ab47a0ec361e2940
zhanggb <zhanggb@motorola.com>
2021-10-03 11:49:03 +0800

    kernel dp: remove calling of fsa4480
    As we use own dp aux switcher but QC call fsa4480 function always,
    remove it temporary.
    
    Change-Id: Ie39c893574356019461c88153edee702e4e25480
    Reviewed-on: https://gerrit.mot.com/2082014
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Yinjun Chen <a7301c@motorola.com>
    Submit-Approved: Yinjun Chen <a7301c@motorola.com>
    Tested-by: Yinjun Chen <a7301c@motorola.com>

--- msm/dp/dp_display.c
+++ msm/dp/dp_display.c
@@ -1383,8 +1383,8 @@ static int dp_display_init_aux_switch(struct dp_display_private *dp)
 {
 	int rc = 0;
 	struct notifier_block nb;
-	const u32 max_retries = 50;
-	u32 retry;
+	//const u32 max_retries = 50;
+	//u32 retry;
 
 	if (dp->aux_switch_ready)
 	       return rc;
@@ -1398,6 +1398,7 @@ static int dp_display_init_aux_switch(struct dp_display_private *dp)
 	 * Iteratively wait for reg notifier which confirms that fsa driver is probed.
 	 * Bootup DP with cable connected usecase can hit this scenario.
 	 */
+	/*
 	for (retry = 0; retry < max_retries; retry++) {
 		rc = fsa4480_reg_notifier(&nb, dp->aux_switch_node);
 		if (rc == 0) {
@@ -1417,7 +1418,7 @@ static int dp_display_init_aux_switch(struct dp_display_private *dp)
 	}
 
 	fsa4480_unreg_notifier(&nb, dp->aux_switch_node);
-
+	*/
 	SDE_EVT32_EXTERNAL(SDE_EVTLOG_FUNC_EXIT, rc);
 	return rc;
 }
