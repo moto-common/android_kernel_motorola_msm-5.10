baf5ac19d2bc7df3e59a578b999e36e17577f7e3
daiss1 <daiss1@lenovo.com>
2022-01-11 16:22:31 +0800

    Hiphi: Optimize the hbm off effect of DC on of csot panel
    Modify:
    Hiphi Optimize the hbm off effect of DC on of csot+ili panel
    
    Change-Id: I2bf5c405e1ce0a937d9a23b2b198e7b93fcd0018
    Signed-off-by: daiss1 <daiss1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2167191
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_panel.h
+++ msm/dsi/dsi_panel.h
@@ -411,6 +411,9 @@ struct dsi_panel {
 	bool is_panel_dead;
        int paramVersion;
 	int backlight_map_type;
+	bool delect_dc_onoff;
+	bool dc_on;
+
 };
 
 bool dsi_display_all_displays_dead(void);
@@ -561,5 +564,7 @@ int dsi_panel_get_elvss_data(struct dsi_panel *panel);
 int dsi_panel_get_elvss_data_1(struct dsi_panel *panel);
 int dsi_panel_set_elvss_dim_off(struct dsi_panel *panel, u8 val);
 int dsi_panel_parse_elvss_config(struct dsi_panel *panel, u8 elv_vl);
+void mot_update_hbmoff(struct dsi_panel *panel,
+                        struct msm_param_info *param_info);
 
 #endif /* _DSI_PANEL_H_ */
