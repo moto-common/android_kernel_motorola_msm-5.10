905679f03863aa36b0f9c53339520468837372f7
zhanggb <zhanggb@motorola.com>
2022-05-28 14:00:40 +0800

    DP: add more log to debug DP cable detection
    DP training log
    Send_video log
    stream enable/disable log
    
    Change-Id: I1cca85310dfbacc4955ff17304fa5961828cdada
    Reviewed-on: https://gerrit.mot.com/2288994
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jichao Zou <zoujc@motorola.com>
    Submit-Approved: Jira Key

--- msm/dp/dp_display.c
+++ msm/dp/dp_display.c
@@ -1545,7 +1545,7 @@ static void dp_display_stream_disable(struct dp_display_private *dp,
 
 	dp_display_update_dsc_resources(dp, dp_panel, false);
 
-	DP_DEBUG("stream_id=%d, active_stream_cnt=%d, tot_dsc_blks_in_use=%d\n",
+	DP_INFO("stream_id=%d, active_stream_cnt=%d, tot_dsc_blks_in_use=%d\n",
 			dp_panel->stream_id, dp->active_stream_cnt,
 			dp->tot_dsc_blks_in_use);
 
@@ -1718,7 +1718,7 @@ static int dp_display_stream_enable(struct dp_display_private *dp,
 
 	dp_display_update_dsc_resources(dp, dp_panel, true);
 
-	DP_DEBUG("dp active_stream_cnt:%d, tot_dsc_blks_in_use=%d\n",
+	DP_INFO("dp active_stream_cnt:%d, tot_dsc_blks_in_use=%d\n",
 			dp->active_stream_cnt, dp->tot_dsc_blks_in_use);
 
 	return rc;
@@ -2352,6 +2352,10 @@ static int dp_display_set_mode(struct dp_display *dp_display, void *panel,
 		return -EINVAL;
 	}
 
+	DP_INFO("## dp_panel[%dx%d:%d:%d] dp_mode[%dx%d:%d]\n",
+		dp_panel->hdisplay, dp_panel->vdisplay, dp_panel->vrefresh, dp_panel->aspect_ratio,
+		mode->timing.h_active, mode->timing.v_active, mode->timing.refresh_rate);
+
 	dp = container_of(dp_display, struct dp_display_private, dp_display);
 	SDE_EVT32_EXTERNAL(SDE_EVTLOG_FUNC_ENTRY, dp->state,
 			mode->timing.h_active, mode->timing.v_active,
@@ -2541,6 +2545,7 @@ static int dp_display_enable(struct dp_display *dp_display, void *panel)
 
 	dp = container_of(dp_display, struct dp_display_private, dp_display);
 
+	DP_INFO("##\n");
 	SDE_EVT32_EXTERNAL(SDE_EVTLOG_FUNC_ENTRY, dp->state);
 	mutex_lock(&dp->session_lock);
 
@@ -2572,6 +2577,7 @@ static int dp_display_enable(struct dp_display *dp_display, void *panel)
 end:
 	mutex_unlock(&dp->session_lock);
 	SDE_EVT32_EXTERNAL(SDE_EVTLOG_FUNC_EXIT, dp->state, rc);
+	DP_INFO("==\n");
 	return rc;
 }
 
@@ -2677,6 +2683,7 @@ static int dp_display_pre_disable(struct dp_display *dp_display, void *panel)
 		goto end;
 	}
 
+	DP_INFO("\n");
 	dp_display_state_add(DP_STATE_HDCP_ABORTED);
 	cancel_delayed_work_sync(&dp->hdcp_cb_work);
 	if (dp_display_is_hdcp_enabled(dp) &&
@@ -2752,6 +2759,7 @@ static int dp_display_disable(struct dp_display *dp_display, void *panel)
 		goto end;
 	}
 
+	DP_INFO("##\n");
 	dp_display_stream_disable(dp, dp_panel);
 
 	dp_display_state_remove(DP_STATE_HDCP_ABORTED);
@@ -2766,6 +2774,7 @@ static int dp_display_disable(struct dp_display *dp_display, void *panel)
 end:
 	mutex_unlock(&dp->session_lock);
 	SDE_EVT32_EXTERNAL(SDE_EVTLOG_FUNC_EXIT, dp->state);
+	DP_INFO("==\n");
 	return 0;
 }
 
