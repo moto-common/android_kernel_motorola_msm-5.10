6eb1c2229dfec602f913428784eeba368c96a36d
Yongqiang Sun <sunyq3@motorola.com>
2024-01-08 17:05:53 +0800

    tank: s5k3k1 ois firmware early upgrade - camera-kernel [1/2]
    details:
           tank & tank_factory config enable ois firmware early upgrade
    
    Change-Id: I1ca6243674218a9b108c6362043fe60b671e32dd
    Signed-off-by: Yongqiang Sun <sunyq3@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2847026
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: <huangsw2@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- config/tank_factory.mk
+++ config/tank_factory.mk
@@ -5,4 +5,6 @@ ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
 
 # Flags to pass into C preprocessor
 CONFIG_MOT_OIS_DW9784_DRIVER := y
+CONFIG_MOT_OIS_EARLY_UPGRADE_FW := y
 ccflags-y += -DCONFIG_MOT_OIS_DW9784_DRIVER=1
+ccflags-y += -DCONFIG_MOT_OIS_EARLY_UPGRADE_FW=1
