27984f7cf47f9b16eb4efee95378152ea129d9dc
betaxab <betaxab@gmail.com>
2024-02-18 20:35:48 +0800

    treewide: Fix reported issues by clang
    Signed-off-by: betaxab <betaxab@gmail.com>

--- drivers/soc/qcom/icnss2/qmi.c
+++ drivers/soc/qcom/icnss2/qmi.c
@@ -165,7 +165,7 @@ static int num_of_products(moto_product *list)
 	return num;
 }
 
-static int get_moto_device()
+static int get_moto_device(void)
 {
 	char *bootdevice = NULL;
 	int rc = 0;
@@ -179,7 +179,7 @@ static int get_moto_device()
 	}
 }
 
-static int get_moto_radio()
+static int get_moto_radio(void)
 {
 	char *radiodevice = NULL;
 	int rc = 0;
